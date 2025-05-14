f6b0540fead2146898e97a155271bca520e64d97
manju766 <mallikarjunmalli51946@gmail.com>
2025-05-14 09:37:53 +0530

    java file add

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.err.println("Calculation:");
+        int a=90;
+        int b=29;
+        int result=a+b;
+
+        System.err.println(result);
+    }
+    
+}
